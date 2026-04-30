add10e8242cc3b0df4bfc1738dbabac16b1bc972
100383Javier <lisjavierbuitragoospina01@gmail.com>
2026-04-29 20:56:44 -0500

    feat(domain): add use cases layer

---
+++ lib/domain/use_cases/toggle_favorite_use_case.dart
@@ -0,0 +1,10 @@
+import 'package:app_descuento/data/repositories/discount_repository.dart';
+
+class ToggleFavoriteUseCase {
+  final IDiscountRepository _repository;
+  ToggleFavoriteUseCase(this._repository);
+
+  Future<void> call(int id, bool currentValue) async {
+    await _repository.toggleFavorite(id, !currentValue);
+  }
+}
